7b44ac7a02c1c0f3cbe7a412931239ab07cb0641
benzeen <benzeen55@gmail.com>
2018-11-29 15:27:35 +0700

    adminQueue

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_key()
 
 @app.route('/')
 def hello():
-    return render_template('customerQR.html', name=create_queue())
+    return render_template('adminQueue.html', name=create_queue())
 
 @app.route('/decode/<token>')  # test purpose only
 def decode(token):
