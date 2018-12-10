58367247ea52b89de22113485c2f2ba221bbd125
Preeti Yuankrathok <preetisatit@gmail.com>
2018-12-10 22:07:40 +0700

    add form, login process

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, url_for
+from flask_login import LoginManager, login_user, logout_user, login_required
+from passlib.hash import bcrypt
 
+from forms import LoginForm
 from functions.config import load_config
 from functions.key import load_key
 from functions.queue import create_queue, get_queue
-from models import db
+from models import Manager, db
 
 
 app = Flask(__name__)
@@ -22,29 +25,53 @@ app.config.update(
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
 )
 
-# database
+# init app
 with app.app_context():
+    # database
     db.app = app
     db.init_app(app)
+
+    # login manager
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
 # load config/key
 load_config()
 load_key()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Manager.query.get(user_id)
 
 @app.route('/')
 def index():
     return render_template('customerQueue.html')
 
 @app.route('/qr')
-def qr_page():
+def qr():
     return render_template('customerQR.html')
 
-@app.route('/login')
-def login_page():
-    return render_template('login.html')
+@app.route('/login', methods=['GET', 'POST'])
+def _login():
+    form = LoginForm()
+
+    # submit form
+    if form.validate_on_submit():
+        # get user
+        manager = Manager.query.filter(Manager.username == form.username.data).first()
+
+        # verify password if user is exists
+        if manager is not None and bcrypt.verify(form.password.data, manager.password):
+            login_user(manager)
+            flash('Log in successfully!')
+            return redirect(url_for('admin'))
+        flash('Invalid username or password!')
+
+    # GET
+    return render_template('login.html', form=form)
 
 @app.route('/admin')
-def admin_page():
+def admin():
     return render_template('adminQueue.html')
 
 @app.route('/decode/<token>')  # test purpose only
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
 
-class Manager(db.Model):
+class Manager(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     alias = db.Column(db.String(255), nullable=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
