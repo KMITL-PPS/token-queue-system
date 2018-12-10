4415da54b9b786e262cd58ed7891c58b8c6ae1d0
Preeti Yuankrathok <preetisatit@gmail.com>
2018-12-10 21:21:29 +0700

    create manager script

---
+++ create_manager.py
@@ -0,0 +1,28 @@
+from getpass import getpass
+
+from passlib.hash import bcrypt
+
+from app import db
+from models import Manager
+
+
+if __name__ == '__main__':
+    alias = input('Alias: ')
+    username = input('Username: ')
+    password_input = getpass('Password: ')
+    password_confirm_input = getpass('Password (again): ')
+
+    # check two password
+    if password_input != password_confirm_input:
+        print('Password do not match!')
+        exit(0)
+
+    # encrypt password
+    password = bcrypt.hash(password_input)
+
+    # create and commit model
+    manager = Manager(alias=alias, username=username, password=password)
+    db.session.add(manager)
+    db.session.commit()
+
+    print('Success!')
